54f0c98ffbda62018bfe215a158e8e6b9d2e1e31
Krishna kashyap <krishnakashyap9997@gmail.com>
2026-06-09 11:45:16 +0530

    contains duplicate

---
+++ week 1/Day 2/contains_duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int val : nums) {
+            if (s.find(val) != s.end()) {
+                return true; 
+            }
+            s.insert(val);
+        }
+        return false; 
+    }
+};
